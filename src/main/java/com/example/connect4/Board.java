bd6a7a369696c5605c682fcb0616e0a9be4f077d
aymanm419 <aymanm419@gmail.com>
2021-11-24 16:26:01 +0200

    Change to column

--- src/main/java/com/example/connect4/Board.java
+++ src/main/java/com/example/connect4/Board.java
@@ -1,10 +1,11 @@
 package com.example.connect4;
 
 import java.util.List;
+import java.util.Stack;
 
 public interface Board {
-    Piece[][] getBoard();
-    boolean isPositionEmpty(int row,int col);
-    void addChip(Piece piece);
-    void removeChip(int row, int col);
+    List<List<Piece>> getBoard();
+    boolean isColumnEmpty(int col);
+    void addChip(Piece piece,int col);
+    void removeChip(int col);
 }
