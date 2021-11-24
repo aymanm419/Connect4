53a05754233861ea13b1d367cf8b9ebfac03120a
aymanm419 <aymanm419@gmail.com>
2021-11-24 16:21:35 +0200

    Change board to 2D array

--- src/main/java/com/example/connect4/Board.java
+++ src/main/java/com/example/connect4/Board.java
@@ -3,7 +3,7 @@ package com.example.connect4;
 import java.util.List;
 
 public interface Board {
-    List<List<Piece>> getBoard();
+    Piece[][] getBoard();
     boolean isPositionEmpty(int row,int col);
     void addChip(Piece piece);
     void removeChip(int row, int col);
